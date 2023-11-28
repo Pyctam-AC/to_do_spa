578ab1e91a0f3fbe3a015a84cdf5cb4da97e3a6d
Rustam Sultangaliev <sultangaliev-ru@yandex.ru>
2023-11-29 01:21:04 +0500

    created get Tasks

--- src/views/App/App.tsx
+++ src/views/App/App.tsx
@@ -1,9 +1,42 @@
-import React from "react"
+import React, {useEffect} from "react";
+
+import { useToDoStore } from "../../data/store/useToDoStore";
+
+import styles from './App.module.scss';
 
 const  App:React.FC = () => {
-  
+
+  console.log(useToDoStore)
+
+  const [
+    tasks,
+    createTask,
+    updateTask,
+    removeTask
+  ] = useToDoStore(state => [
+    state.tasks,
+    state.createTask,
+    state.updateTask,
+    state.removeTask,
+  ])
+
+  useEffect (() => {
+    createTask('fmfkdldl')
+  }, [])
+
+  console.log(11, tasks)
+
   return (
-    <h1>TO DO</h1>
+    <article className={styles.article}>
+      <h1 className={styles.articleTitle}>TO DO</h1>
+      <section className={styles.articleSection}>
+
+      </section>
+      <section className={styles.articleSection}>
+
+      </section>
+    </article>
+
   )
 }
 
